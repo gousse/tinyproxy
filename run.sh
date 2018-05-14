f02a996affdb7bed55bb46e7443b7544a309da05
Bruno Munoz <bruno.munoz@orange.com>
2018-05-14 16:17:54 +0200

    update run

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-docker container stop tinyproxy >/dev/null
-docker container rm tinyproxy >/dev/null
+
+source $DIR/env.sh
+docker network create proxy 2>/dev/null
+
+docker container stop tinyproxy 2>/dev/null
+docker container rm tinyproxy 2>/dev/null
 docker container run --name tinyproxy \
   --restart always \
   -v $DIR/config:/etc/tinyproxy \
+  --network=proxy \
+  -p $TINY_LISTEN_IP:8888:8888 \
   -p 127.0.0.1:8888:8888 \
   -d \
   tinyproxy:latest
